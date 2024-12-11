987a44fabdb7035093013d780532e64f22a52b3a
Hiroki Mizukami <hiroki@Hirokis-MacBook-Pro.local>
2024-12-11 16:30:11 +0900

    fix: udf

--- openaivec/udf.py
+++ openaivec/udf.py
@@ -1,23 +1,30 @@
+import os
 from typing import Iterator
 
+import httpx
 import pandas as pd
-from openai import OpenAI
+from openai import AzureOpenAI
 from pyspark.sql.pandas.functions import pandas_udf
 from pyspark.sql.types import StringType
 
 from openaivec import VectorizedOpenAI
 
 
-def openai_udf(client: OpenAI, model_name: str, system_message: str, top_p: float = 1.0, temperature: float = 0.0,
-               batch_size: int = 128):
+def openai_udf(system_message: str, batch_size: int = 128):
     @pandas_udf(StringType())
     def fn(col: Iterator[pd.Series]) -> Iterator[pd.Series]:
+        client = AzureOpenAI(
+            api_version=os.environ.get("OPENAI_API_VERSION", "2024-10-21"),
+            azure_endpoint=os.environ.get("OPENAI_AZURE_ENDPOINT"),
+            http_client=httpx.Client(http2=True, verify=False),
+        )
+
         client_vec = VectorizedOpenAI(
             client=client,
-            model_name=model_name,
+            model_name=os.environ.get("OPENAI_MODEL_NAME"),
             system_message=system_message,
-            top_p=top_p,
-            temperature=temperature,
+            top_p=1.0,
+            temperature=0.0,
         )
 
         for part in col:
