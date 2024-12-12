6644d989485a9ee5aa039283f76a50c3e4bff21e
Hiroki Mizukami <hiroki@Hirokis-MacBook-Pro.local>
2024-12-12 10:18:45 +0900

    fix: bug

--- openaivec/__init__.py
+++ openaivec/__init__.py
@@ -1,7 +1,8 @@
+from .udf import openai_udf, UDFConfig
 from .vectorize import VectorizedOpenAI
-from .udf import openai_udf
 
 __ALL__ = [
     "VectorizedOpenAI",
     "openai_udf",
+    "UDFConfig",
 ]
