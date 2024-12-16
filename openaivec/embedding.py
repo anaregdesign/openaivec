5d549df79cf945407119caa80efb4585959df5a8
Hiroki Mizukami <hiroki@Hirokis-MacBook-Pro.local>
2024-12-16 16:57:02 +0900

    fix: typing

--- openaivec/embedding.py
+++ openaivec/embedding.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 from typing import List
 
+import numpy as np
+from numpy.typing import NDArray
 from openai import OpenAI
 
 from openaivec.util import map_with_minibatch
@@ -11,9 +13,9 @@ class EmbeddingOpenAI:
     client: OpenAI
     model_name: str
 
-    def embed(self, sentences: List[str]) -> List[List[float]]:
+    def embed(self, sentences: List[str]) -> List[NDArray[np.float32]]:
         responses = self.client.embeddings.create(input=sentences, model=self.model_name)
-        return [d["embedding"] for d in responses.data]
+        return [np.ndarray(d["embedding"], dtype=np.float32) for d in responses.data]
 
-    def embed_minibatch(self, sentences: List[str], batch_size: int) -> List[List[float]]:
+    def embed_minibatch(self, sentences: List[str], batch_size: int) -> List[NDArray[np.float32]]:
         return map_with_minibatch(sentences, batch_size, self.embed)
