72456424431272bf5644c9800d05677dfc665c83
Hiroki Mizukami <hiroki@Hirokis-MacBook-Pro.local>
2025-03-16 16:07:18 +0900

    reformat: with ruff

--- openaivec/__init__.py
+++ openaivec/__init__.py
@@ -1,7 +1,7 @@
 from .embedding import EmbeddingOpenAI
 from .vectorize import VectorizedOpenAI
 
-__ALL__ = [
+__all__ = [
     "VectorizedOpenAI",
     "EmbeddingOpenAI",
 ]
--- openaivec/util.py
+++ openaivec/util.py
@@ -88,13 +88,13 @@ def python_type_to_spark(python_type):
         return pydantic_to_spark_schema(python_type)
 
     # Basic type mapping
-    elif python_type == int:
+    elif python_type is int:
         return IntegerType()
-    elif python_type == float:
+    elif python_type is float:
         return FloatType()
-    elif python_type == str:
+    elif python_type is str:
         return StringType()
-    elif python_type == bool:
+    elif python_type is bool:
         return BooleanType()
     else:
         raise ValueError(f"Unsupported type: {python_type}")
