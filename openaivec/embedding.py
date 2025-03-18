961a7a788116e5b09d240ceb71fff8354bf786cd
Hiroki Mizukami <hiroki@Hirokis-MacBook-Pro.local>
2025-03-18 17:56:47 +0900

    fix: handle rate limit

--- openaivec/embedding.py
+++ openaivec/embedding.py
@@ -1,10 +1,11 @@
+import time
 from dataclasses import dataclass
 from logging import Logger, getLogger
 from typing import List
 
 import numpy as np
 from numpy.typing import NDArray
-from openai import OpenAI
+from openai import OpenAI, RateLimitError
 
 from openaivec.log import observe
 from openaivec.util import map_unique_minibatch_parallel
@@ -21,7 +22,13 @@ class EmbeddingOpenAI:
 
     @observe(_logger)
     def embed(self, sentences: List[str]) -> List[NDArray[np.float32]]:
-        responses = self.client.embeddings.create(input=sentences, model=self.model_name)
+        while True:
+            try:
+                responses = self.client.embeddings.create(input=sentences, model=self.model_name)
+                break
+            except RateLimitError:
+                _logger.info("429 RateLimit encountered; retrying in 60 seconds")
+                time.sleep(60)
         return [np.array(d.embedding, dtype=np.float32) for d in responses.data]
 
     @observe(_logger)
