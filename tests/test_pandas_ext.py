c60bdf28c52bee85775bf235c83b5fd8ad376186
Hiroki Mizukami <hiroki@Hirokis-MacBook-Pro.local>
2025-03-21 23:54:25 +0900

    chore: edit example

---
+++ tests/test_pandas_ext.py
@@ -0,0 +1,27 @@
+import unittest
+
+import numpy as np
+import pandas as pd
+
+from openaivec import pandas_ext
+
+
+class TestPandasExt(unittest.TestCase):
+    def setUp(self):
+        self.df = pd.DataFrame(
+            {
+                "name": ["apple", "banana", "cherry"],
+            }
+        )
+
+    def test_embed(self):
+        embeddings: pd.Series = self.df["name"].openaivec.embed("text-embedding-3-large")
+
+        # assert all values are elements of np.ndarray
+        self.assertTrue(all(isinstance(embedding, np.ndarray) for embedding in embeddings))
+
+    def test_predict(self):
+        names_fr: pd.Series = self.df["name"].openaivec.predict("gpt-4o-mini", "translate to French")
+
+        # assert all values are elements of str
+        self.assertTrue(all(isinstance(name_fr, str) for name_fr in names_fr))
