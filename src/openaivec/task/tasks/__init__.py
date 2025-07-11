4642efa3b91259d3582ebb3756af4dff27498832
Hiroki Mizukami <6128022+piroyoung@users.noreply.github.com>
2025-07-11 20:59:18 +0900

    feature: typical NLP task

--- src/openaivec/task/tasks/__init__.py
+++ src/openaivec/task/tasks/__init__.py
@@ -1,5 +1,15 @@
 from .translation import MULTILINGUAL_TRANSLATION_TASK
+from .morphological_analysis import MORPHOLOGICAL_ANALYSIS_TASK
+from .named_entity_recognition import NAMED_ENTITY_RECOGNITION_TASK
+from .sentiment_analysis import SENTIMENT_ANALYSIS_TASK
+from .dependency_parsing import DEPENDENCY_PARSING_TASK
+from .keyword_extraction import KEYWORD_EXTRACTION_TASK
 
 __all__ = [
     "MULTILINGUAL_TRANSLATION_TASK",
+    "MORPHOLOGICAL_ANALYSIS_TASK",
+    "NAMED_ENTITY_RECOGNITION_TASK",
+    "SENTIMENT_ANALYSIS_TASK",
+    "DEPENDENCY_PARSING_TASK",
+    "KEYWORD_EXTRACTION_TASK",
 ]
